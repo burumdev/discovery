4f060c75fa36bb214e3f589249bd3a636d7eab42
burumdev <burum.gh@gmail.com>
2025-08-02 19:41:47 +0300

    Timer 6 leds

--- f3discovery/src/09-clocks-and-timers/src/main.rs
+++ f3discovery/src/09-clocks-and-timers/src/main.rs
@@ -5,7 +5,15 @@ use aux9::{entry, switch_hal::OutputSwitch, tim6};
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // TODO implement this
+    // Set the timer to go off in `ms` ticks
+    // 1 tick = 1 ms
+    tim6.arr.write(|w| w.arr().bits(ms));
+    // CEN: Enable the counter
+    tim6.cr1.modify(|_, w| w.cen().set_bit());
+    // Wait until the alarm goes off (until the update event occurs)
+    while !tim6.sr.read().uif().bit_is_set() {}
+    // Clear the update event flag
+    tim6.sr.modify(|_, w| w.uif().clear_bit());
 }
 
 #[entry]
@@ -14,16 +22,33 @@ fn main() -> ! {
     let mut leds = leds.into_array();
 
     // TODO initialize TIM6
+    // Enable power to timer counter 6
+    rcc.apb1enr.write(|w| w.tim6en().set_bit());
+    // Timer 6 control register opm bit controls one pulse mode
+    // We enable it for one pulse mode.
+    // CEN bit controls counter enable.
+    // We keep it disabled for now.
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
 
-    let ms = 50;
+    // Configure prescaler so that counter operates at 1 khz (1 millisecond period)
+    // 0.001 seconds * 8_000_000 = 8000 ticks per millisecond
+    // But starts from 0 so 7999
+    tim6.psc.write(|w| w.psc().bits(7999));
+
     loop {
-        for curr in 0..8 {
-            let next = (curr + 1) % 8;
+        let mut ms = 1024;
+        for _ in 0..10 {
+            // Gradually increase the speed of revolutions by 10 steps by diving by 2 (2^10 = 1024)
+            ms /= 2;
 
-            leds[next].on().unwrap();
-            delay(tim6, ms);
-            leds[curr].off().unwrap();
-            delay(tim6, ms);
+            for curr in 0..8 {
+                let next = (curr + 1) % 8;
+
+                leds[next].on().unwrap();
+                delay(tim6, ms);
+                leds[curr].off().unwrap();
+                delay(tim6, ms);
+            }
         }
     }
 }
