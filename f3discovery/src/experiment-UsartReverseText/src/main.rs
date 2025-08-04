9ad68a7987c55d7c298d25d68da7f572b7c1ef28
burumdev <burum.gh@gmail.com>
2025-08-04 17:10:42 +0300

    Better usart text reverser

--- f3discovery/src/experiment-UsartReverseText/src/main.rs
+++ f3discovery/src/experiment-UsartReverseText/src/main.rs
@@ -11,6 +11,14 @@ use uprintln::*;
 
 use heapless::Vec;
 
+fn reverse_text(serial: &mut SerialPort, buffer: &Vec<u8, 64>) {
+    uprint!(serial, "\n\r");
+    for ch in buffer.iter().rev() {
+        uprint!(serial, "{}", *ch as char);
+    }
+    uprint!(serial, "\n\r");
+}
+
 #[entry]
 fn main() -> ! {
     let (usart1, _mt, _itm) = aux114::init();
@@ -18,6 +26,11 @@ fn main() -> ! {
     let mut serial = SerialPort { usart: usart1 };
 
     let mut buffer: Vec<u8, 64> = Vec::new();
+
+    uprintln!(
+        serial,
+        "Uprintln and friends at your service. Type something calmly and press ENTER to have stm32 reverse it. I said CALMLY!\n\r* This crashes if you type so fast..\n\r* Or you type evil utf8 characters..\n\r",
+    );
 
     loop {
         buffer.clear();
@@ -29,24 +42,29 @@ fn main() -> ! {
 
             let ch: u8 = serial.usart.rdr.read().bits() as u8;
             if ch != 13 {
-                // Key code is ENTER
+                // Keycode is NOT Enter
                 if buffer.push(ch).is_ok() {
                     uprint!(serial, "{}", ch as char);
                 } else {
                     uprintln!(
                         serial,
-                        "Hey! We couldn't write {} to buffer! This is an ERROR! Exiting.",
-                        ch
+                        "\n\rOoops! We couldn't write '{}' to buffer. Buffer might be full. So let's interpret it as Enter key pressed.",
+                        ch as char
                     );
-                    loop {}
+                    // We execute the reverse operation as
+                    // if user pressed enter.
+                    reverse_text(&mut serial, &buffer);
+                    // Restore the intended character to a clear buffer
+                    // as a workaround of buffer overflow situation.
+                    uprint!(serial, "{}", ch as char);
+                    buffer.clear();
+                    buffer.push(ch).unwrap();
+
+                    continue 'inner;
                 }
             } else {
-                // Key code is not ENTER, so something else
-                uprint!(serial, "\n\r");
-                for ch in buffer.iter().rev() {
-                    uprint!(serial, "{}", *ch as char);
-                }
-                uprint!(serial, "\n\r");
+                // Key code is ENTER
+                reverse_text(&mut serial, &buffer);
                 break 'inner;
             }
         }
